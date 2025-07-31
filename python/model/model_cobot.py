21b2533e52a4066c737e595b997378f5b1723c74
martin federico sanchez <mfsanchez@inti.gob.ar>
2025-07-31 14:47:47 -0300

    .

--- python/model/model_cobot.py
+++ python/model/model_cobot.py
@@ -287,11 +287,8 @@ class ModelCobot(QObject):
     def enviar_ordenes(self,mensaje: list, condicion_loop: bool, RPM: str):
         try:
             if condicion_loop == False:
-                print(float(RPM))
                 delay_microseconds = round(60 * 10**6 / (float(RPM) * 200))
-                print(delay_microseconds)
                 set_cantindad_mov = f"Set_mov{delay_microseconds}_{len(mensaje)}" 
-                print(set_cantindad_mov)
                 self.ser.write(set_cantindad_mov.encode())
                 time.sleep(0.5)  # espero por las dudas
                 self.espera_correcta_recepcion(set_cantindad_mov, True)
